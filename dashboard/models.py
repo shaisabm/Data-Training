c91b8c3e20e67d01105a29030d1dde4f735da0e9
Shaisab Mistry <shaisabmistry@gmail.com>
2024-10-09 16:30:29 -0400

    made changes in the database

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,32 +2,28 @@ from django.db import models
 
 # Create your models here.
 
-class Event(models.Model):
-    event_name = models.CharField(max_length=100, null=True, blank=True)
-    zoom_id = models.CharField(max_length=100, unique=True)
 
 
 class Registration(models.Model):
-    event_name = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='event_registrations')
-    zoom_id = models.ForeignKey(Event, on_delete=models.CASCADE ,related_name='zoom_registrations')
+    event_name = models.CharField(max_length=500, null=True, blank=True)
+    zoom_id = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    registration_time = models.DateTimeField()
-    approved = models.BooleanField(default=False)
-    status = models.CharField(max_length=100)
+    registration_time = models.CharField()
+    approval_status = models.CharField(max_length=100)
     participated = models.BooleanField(default=False, null=True, blank=True)
 
 
 
 class Participant(models.Model):
-    zoom_id = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='zoom_participants')
-    event_name = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='event_participants')
+    event_name = models.CharField(max_length=500, null=True, blank=True)
+    zoom_id = models.CharField(max_length=100)
     full_name = models.CharField(max_length=100)
     email = models.EmailField()
     joined_time = models.DateTimeField()
     leave_time = models.DateTimeField()
-    duration = models.DurationField()
+    duration = models.IntegerField(default=0)
     guest = models.BooleanField(default=False)
     in_waiting_room = models.BooleanField(default=False)
 
