239da2a19d6d2e6cac359cbdc9d0dd894260dd84
Shaisab Mistry <shaisabmistry@gmail.com>
2025-02-12 11:54:21 -0500

    Added date, month and time columns

--- dashboard/master_to_db/master_to_db.py
+++ dashboard/master_to_db/master_to_db.py
@@ -7,17 +7,19 @@ def master_to_db(master_df):
     for index, row in master_df.iterrows():
         if str(row['duration']) == 'nan':
             duration = 0
-            print('duration is nan')
         else:
             duration = row['duration']
 
         email = row['email']
+        print(row["event month"])
         if ExcludedIndividual.objects.filter(email=email).exists():
             continue
         MasterDB.objects.update_or_create(
             topic = row['topic'],
             zoom_id = row['id'],
-            event_date = row['event date'],
+            event_month = row['event month'],
+            event_date=row['event date'],
+            event_time=row['event time'],
             first_name = row['first name'],
             last_name = row['last name'],
             email = row['email'],
