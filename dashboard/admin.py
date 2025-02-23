b463b51cb855c9478f119f16d50b03f387d669a1
Shaisab Mistry <shaisabmistry@gmail.com>
2025-02-22 22:41:59 -0500

    added default config tab in admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AiModel
+from .models import AiModel, DefaultAiConfig
 # Register your models here.
 
 admin.site.register(AiModel)
+admin.site.register(DefaultAiConfig)
