82a227ecb0f2f4b42203fd7e584da22210050a53
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-01 23:25:10 -0500

    add CELERYD_USER and CELERYD_GROUP configuration for Celery worker

--- DataTraining/celery.py
+++ DataTraining/celery.py
@@ -12,6 +12,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+CELERYD_USER="celery"
+CELERYD_GROUP="celery"
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
