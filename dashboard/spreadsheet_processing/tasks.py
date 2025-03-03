4f576bece8a9de4cf258411db92938dfd8c66d86
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-03 02:04:09 -0500

    refactor process_ai_models_async to improve logging and remove unused code

--- dashboard/spreadsheet_processing/tasks.py
+++ dashboard/spreadsheet_processing/tasks.py
@@ -8,29 +8,22 @@ import base64
 
 @shared_task
 def process_ai_models_async(matched_pairs, ai_models_ids, model_config):
-    print("process ai models running")
+    print("Celery is running")
     ai_models = AiModel.objects.filter(id__in=ai_models_ids)
 
     for reg, part in matched_pairs:
 
-        # with default_storage.open(reg) as reg_file, default_storage.open(part) as part_file:
         for model in ai_models:
             reg_content = reg['content']
             part_content = part['content']
             try:
                 result = open_ai(reg_content, part_content, model, model_config)
-                # print("Succeed: ", reg)
+                print("Succeed: ", reg)
                 print(result)
                 break
             except Exception as e:
                 print(f"Failed: {reg['name']} - {str(e)}")
                 continue
-            finally:
-                pass
-                # if reg:
-                #     default_storage.delete(reg)
-                # if part:
-                #     default_storage.delete(part)
 
 
 
