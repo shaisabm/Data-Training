32b8ad5a333ec1cbc0c552ef244bbd92132fb342
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-08 00:52:37 -0500

    refactor main.py to optimize common code extraction logic

--- dashboard/spreadsheet_processing/main.py
+++ dashboard/spreadsheet_processing/main.py
@@ -1,6 +1,5 @@
 import re
 
-
 def extract_code(filename):
     filename = filename
     match = re.search(r'(\d{11})', filename)
@@ -20,7 +19,7 @@ def match_registration_participant_files(registration_files, participant_files):
     for file in part_files:
         part_dict[extract_code(file.name)] = file
 
-    common_codes = set(reg_dict.keys()) & set(part_dict.keys())
+    common_codes = set(reg_dict.keys() & part_dict.keys())
     missing_files = []
     for file in all_files:
         if extract_code(file.name) not in common_codes:
@@ -31,4 +30,7 @@ def match_registration_participant_files(registration_files, participant_files):
     for code in common_codes:
         matched_pairs.append((reg_dict[code], part_dict[code]))
 
+
     return {"matched_pairs": matched_pairs, "missing_files": missing_files}
+
+
