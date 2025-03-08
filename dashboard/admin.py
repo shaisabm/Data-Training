63b1be55187da98f87ae752b7afa16f5ee43f422
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-07 21:50:20 -0500

    register MasterDB model in admin panel

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import AiModel, DefaultAiConfig
+from .models import AiModel, DefaultAiConfig, MasterDB
 # Register your models here.
 
 admin.site.register(AiModel)
 admin.site.register(DefaultAiConfig)
+admin.site.register(MasterDB)
+
