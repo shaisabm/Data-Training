50e6fb776b4a321d558c5d04024504f71b035426
Shaisab Mistry <shaisabmistry@gmail.com>
2025-03-13 20:02:49 -0400

    refactor views.py to clean up commented-out code and improve readability

--- dashboard/views.py
+++ dashboard/views.py
@@ -16,6 +16,7 @@ from rest_framework.decorators import api_view
 from .spreadsheet_processing.main import match_registration_participant_files
 from .spreadsheet_processing.celery_worker import process_ai_models_async, save_for_celery
 
+
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('home')
@@ -38,66 +39,62 @@ def logout_user(request):
     return redirect('login')
 
 
-
-
 @login_required(login_url='/login')
 def home(request):
-
-        # OLD CODE START
-        # try:
-        #     participant_files = request.FILES.getlist('participation_files')
-        #     del request.FILES['participation_files']
-        # except:
-        #     participant_files = []
-        #
-        # request_dict = dict(request.FILES)
-        # registration_files = []
-        #
-        # for key in request.FILES.keys():
-        #     for file in request_dict[key]:
-        #         base_name = str(file).split('_')[:2]
-        #         new_file = InMemoryUploadedFile(
-        #             file=file,
-        #             field_name=file.field_name,
-        #             name=f"{base_name[0]}_{base_name[1]}_{key}.csv",
-        #             content_type=file.content_type,
-        #             size=file.size,
-        #             charset=file.charset,
-        #             content_type_extra=file.content_type_extra
-        #
-        #         )
-        #         registration_files.append(new_file)
-        #
-        # participant_dict = {str(participant_file).split('_')[1][:11]: participant_file for participant_file in
-        #                     participant_files}
-        # registration_dict = {}
-        # for f in registration_files:
-        #     id = str(f).split('_')[1]
-        #     registration_dict[id] = f
-        #
-        # keys_to_remove = []
-        # for r_key in registration_dict.keys():
-        #     if r_key not in participant_dict.keys():
-        #         keys_to_remove.append(r_key)
-        #
-        #
-        # for p_key in participant_dict.keys():
-        #     if p_key not in registration_dict.keys():
-        #         keys_to_remove.append(p_key)
-        #
-        # for key in keys_to_remove:
-        #     if (key in registration_dict.keys()):
-        #         registration_dict.pop(key)
-        #     if (key in participant_dict.keys()):
-        #         participant_dict.pop(key)
-        #
-        # for key in registration_dict.keys():
-        #     registration_file = registration_dict[key]
-        #     participant_file = participant_dict[key]
-        #     master_df = data_cleaning(registration_file, participant_file)
-        #     master_to_db(master_df)
-        # OLD CODE END
-
+    # OLD CODE START
+    # try:
+    #     participant_files = request.FILES.getlist('participation_files')
+    #     del request.FILES['participation_files']
+    # except:
+    #     participant_files = []
+    #
+    # request_dict = dict(request.FILES)
+    # registration_files = []
+    #
+    # for key in request.FILES.keys():
+    #     for file in request_dict[key]:
+    #         base_name = str(file).split('_')[:2]
+    #         new_file = InMemoryUploadedFile(
+    #             file=file,
+    #             field_name=file.field_name,
+    #             name=f"{base_name[0]}_{base_name[1]}_{key}.csv",
+    #             content_type=file.content_type,
+    #             size=file.size,
+    #             charset=file.charset,
+    #             content_type_extra=file.content_type_extra
+    #
+    #         )
+    #         registration_files.append(new_file)
+    #
+    # participant_dict = {str(participant_file).split('_')[1][:11]: participant_file for participant_file in
+    #                     participant_files}
+    # registration_dict = {}
+    # for f in registration_files:
+    #     id = str(f).split('_')[1]
+    #     registration_dict[id] = f
+    #
+    # keys_to_remove = []
+    # for r_key in registration_dict.keys():
+    #     if r_key not in participant_dict.keys():
+    #         keys_to_remove.append(r_key)
+    #
+    #
+    # for p_key in participant_dict.keys():
+    #     if p_key not in registration_dict.keys():
+    #         keys_to_remove.append(p_key)
+    #
+    # for key in keys_to_remove:
+    #     if (key in registration_dict.keys()):
+    #         registration_dict.pop(key)
+    #     if (key in participant_dict.keys()):
+    #         participant_dict.pop(key)
+    #
+    # for key in registration_dict.keys():
+    #     registration_file = registration_dict[key]
+    #     participant_file = participant_dict[key]
+    #     master_df = data_cleaning(registration_file, participant_file)
+    #     master_to_db(master_df)
+    # OLD CODE END
 
     if request.method == "POST":
 
@@ -117,10 +114,11 @@ def home(request):
 
         process_ai_models_async.delay(matched_pairs)
 
-        data = MasterDB.objects.all().values(
-        'topic','zoom_id', 'event_date', 'first_name', 'last_name', 'email', 'join_time', 'leave_time',
+    data = MasterDB.objects.all().values(
+        'topic', 'zoom_id', 'event_date', 'first_name', 'last_name', 'email', 'join_time', 'leave_time',
         'duration', 'attended'
     )
+
     BASE_DIR = Path(__file__).resolve().parent.parent
     print(BASE_DIR)
 
@@ -149,7 +147,6 @@ def comparison(request):
 
 @login_required(login_url='/login')
 def visualization(request):
-
     return render(request, 'dashboard/visualization.html')
 
 
